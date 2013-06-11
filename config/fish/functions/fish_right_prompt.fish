e7ade51733922f676a0d57f79fec8863d36ee43c
David Celis <me@davidcel.is>
2013-06-10 23:23:49 -0700

    Make the rbenv version red
    Signed-off-by: David Celis <me@davidcel.is>

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,7 @@
 function fish_right_prompt
   if [ (which rbenv ^/dev/null) ]
-    echo (rbenv version-name)
+    set -l red (set_color red)
+    set -l normal (set_color normal)
+    echo "$red"(rbenv version-name)"$normal"
   end
 end
