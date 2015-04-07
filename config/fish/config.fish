d224f2383f143e79ac13ac29db22e3c689b53e4b
David Celis <me@davidcel.is>
2015-04-07 18:45:40 -0400

    Update docker information
    Signed-off-by: David Celis <me@davidcel.is>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -32,9 +32,9 @@ if [ -z "$TMUX" ]
   set -x JRUBY_OPTS "--2.0 -Xcext.enabled=true"
 
   # Docker
-  set -x DOCKER_CERT_PATH /Users/david/.boot2docker/certs/boot2docker-vm
+  set -x DOCKER_CERT_PATH /Users/david/.boot2docker/certs/boot2docker
   set -x DOCKER_TLS_VERIFY 1
-  set -x DOCKER_HOST tcp://192.168.59.103:2376
+  set -x DOCKER_HOST tcp://192.168.59.104:2376
 end
 
 # Ruby
