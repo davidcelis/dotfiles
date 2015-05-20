e1e9f5726fe4d9cb7888afca94f206d90d61a955
David Celis <me@davidcel.is>
2015-05-20 14:39:24 -0700

    Move system-specific configuration all to local.fish
    Signed-off-by: David Celis <me@davidcel.is>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,52 +1,28 @@
 # No greeting, please
 set -e fish_greeting
 
-# No need to re-evaluate environment variables in tmux
-if [ -z "$TMUX" ]
-  # Set the Code directory
-  set -x CODE $HOME/Documents/Code
+# Universal
+set -x PATH $HOME/.local/bin $PATH
+set -x EDITOR vim
+set -x VISUAL $EDITOR
 
-  # Local
-  set -x PATH $HOME/.local/bin $PATH
-
-  # Homebrew
-  set -x PATH /usr/local/bin /usr/local/sbin $PATH
-  set -x PATH $PATH /usr/local/share/npm/bin
-
-  # Go
-  set -x GOPATH /usr/local/share/go
-  set -x PATH $PATH $GOPATH/bin
-
-  # Vim
-  set -x EDITOR vim
-  set -x VISUAL $EDITOR
-
-  # Ruby
-  set -x RUBY_GC_HEAP_INIT_SLOTS 1000000
-  set -x RUBY_HEAP_SLOTS_INCREMENT 1000000
-  set -x RUBY_HEAP_SLOTS_GROWTH_FACTOR 1
-  set -x RUBY_GC_MALLOC_LIMIT 100000000
-  set -x RUBY_HEAP_FREE_MIN 500000
-
-  # JRuby 2.0 mode
-  set -x JRUBY_OPTS "--2.0 -Xcext.enabled=true"
-end
+# Set the Code directory
+set -x CODE $HOME/Documents/Code
 
 # Ruby
-source /usr/local/opt/fry/share/fry/fry.fish
-
-eval sh $HOME/.config/base16-shell/base16-tomorrow.dark.sh
+if test -f /usr/local/share/fry/fry.fish
+    source /usr/local/share/fry/fry.fish
+end
 
 # Aliases
-alias ssh-add-all "ssh-add ~/.ssh/*id_rsa"
 alias ag "ag --smart-case"
-alias pianobar "pianokeys; and /usr/local/bin/pianobar"
+alias rm "trash"
 
-if [ (which exa) ]
-  alias ls "exa"
+if test -x (which exa)
+    alias ls "exa"
 end
 
 # Source sensitive configuration
-if [ -f "$HOME/.local/share/config.fish" ]
-  source $HOME/.local/share/config.fish
+if test -f $HOME/.config/fish/local.fish
+    source $HOME/.config/fish/local.fish
 end
