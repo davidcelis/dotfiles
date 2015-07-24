7f674824631653b118f91ad9fca34440313abac7
David Celis <me@davidcel.is>
2015-07-24 15:42:35 -0700

    Add missing completions/functions
    Signed-off-by: David Celis <me@davidcel.is>

---
+++ config/fish/functions/d.fish
@@ -0,0 +1,10 @@
+function d --description 'Like z, but d'
+  cd $DEVELOPER
+  for dir in $argv
+    cd $dir*/
+    or begin
+      echo "fish: No matches for wildcard '$dir*/'." >&2
+      return 1
+    end
+  end
+end
